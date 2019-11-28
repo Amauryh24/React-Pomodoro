98b39107c527826bbd0a556075d4a93798a02508
amaury2409 <amaury.henrottay@gmail.com>
2019-11-28 09:23:15 +0100

    toggle

--- src/components/PomodoroApp.js
+++ src/components/PomodoroApp.js
@@ -1,81 +1,103 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import BreakTime from "./BreakTime"
-import PomodoroTime from "./PomodoroTime"
-import Timer from './Timer'
-
-
+import BreakTime from "./BreakTime";
+import PomodoroTime from "./PomodoroTime";
+import Timer from "./Timer";
 
 class PomodoroApp extends React.Component {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            breakLength : 5,
-            pomodoroLength: 20,
-            timerMinutes:25
-        }  
+    this.state = {
+      breakLength: 5,
+      pomodoroLength: 20,
+      timerMinutes: 25
+    };
 
-        this.upBreakTime = this.upBreakTime.bind(this);
-        this.downBreakTime = this.downBreakTime.bind(this);
+    this.upBreakTime = this.upBreakTime.bind(this);
+    this.downBreakTime = this.downBreakTime.bind(this);
 
-        this.upPomodoroTime = this.upPomodoroTime.bind(this);
-        this.downPomodoroTime = this.downPomodoroTime.bind(this);
+    this.upPomodoroTime = this.upPomodoroTime.bind(this);
+    this.downPomodoroTime = this.downPomodoroTime.bind(this);
+
+    this.onToggleInterval = this.onToggleInterval.bind(this);
+    this.onUpdateTimerMinute = this.onUpdateTimerMinute.bind(this);
+  }
+  upBreakTime() {
+    this.setState(par => {
+      return {
+        breakLength: par.breakLength + 1
+      };
+    });
+  }
+  downBreakTime() {
+    this.setState(par => {
+      return {
+        breakLength: par.breakLength - 1
+      };
+    });
+  }
+  upPomodoroTime() {
+    this.setState(par => {
+      return {
+        pomodoroLength: par.pomodoroLength + 1,
+        timerMinutes: par.pomodoroLength + 1
+      };
+    });
+  }
+  downPomodoroTime() {
+    this.setState(par => {
+      return {
+        pomodoroLength: par.pomodoroLength - 1,
+        timerMinutes: par.pomodoroLength - 1
+      };
+    });
+  }
+  onUpdateTimerMinute() {
+    this.setState(par => {
+      return {
+        timerMinutes: par.timerMinutes - 1
+      };
+    });
+  }
+  onToggleInterval(isPomodoro) {
+    if (isPomodoro) {
+      this.setState({
+        timerMinutes: this.state.pomodoroLength
+      });
+    } else {
+      this.setState({
+        timerMinutes: this.state.breakLength
+      });
     }
-upBreakTime(){
-    this.setState((par) => {
-        return{
-            breakLength: par.breakLength + 1
-            
-        }
-    })
-}
-downBreakTime(){
-    this.setState((par) => {
-        return{
-            breakLength: par.breakLength - 1
-        }
-    })
-}
-upPomodoroTime(){
-    this.setState((par) => {
-        return{
-            pomodoroLength: par.pomodoroLength + 1,
-            timerMinutes: par.pomodoroLength + 1
-        }
-    })
-}
-downPomodoroTime(){
-    this.setState((par) => {
-        return{
-            pomodoroLength: par.pomodoroLength - 1,
-            timerMinutes: par.pomodoroLength - 1
-        }
-    })
-}
+  }
 
-
- render(){
- return (
-        <div>
-            <main>
-            <BreakTime 
+  render() {
+    return (
+      <div>
+        <main>
+          <BreakTime
             break={this.state.breakLength}
-            increaseCounter= { this.upBreakTime}
-            decreaseCounter= { this.downBreakTime} />
+            increaseCounter={this.upBreakTime}
+            decreaseCounter={this.downBreakTime}
+          />
 
-            <PomodoroTime 
-            pomodoro={this.state.pomodoroLength} 
-            increaseCounter= { this.upPomodoroTime}
-            decreaseCounter= { this.downPomodoroTime} />
+          <PomodoroTime
+            pomodoro={this.state.pomodoroLength}
+            increaseCounter={this.upPomodoroTime}
+            decreaseCounter={this.downPomodoroTime}
+          />
 
-            <Timer timerMinutes={this.state.timerMinutes} />
-            </main>
-        </div>
+          <Timer
+            timerMinutes={this.state.timerMinutes}
+            breakLength={this.state.breakLength}
+            UpdateTimerMinute={this.state.onUpdateTimerMinute}
+            ToggleInterval={this.state.onToggleInterval}
+          />
+        </main>
+      </div>
     );
- }
-   
+  }
 }
-
 
 export default PomodoroApp;
